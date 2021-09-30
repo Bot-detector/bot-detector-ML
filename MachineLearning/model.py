ee606f53a0b2cac8d3e9ec770823b9256f4959f5
extreme4all <40169115+extreme4all@users.noreply.github.com>
2021-10-01 01:50:07 +0200

    base training & model layout

--- MachineLearning/model.py
+++ MachineLearning/model.py
@@ -1,9 +1,11 @@
 import time
 
 from joblib import dump, load
-
+import pandas as pd
+from pandas.core.frame import DataFrame
 from MachineLearning.data import data_class
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
 
 class model:
     save_folder = 'MachineLearning/models'
@@ -13,16 +15,41 @@ class model:
     def __init__(self, labels: list) -> None:
         self.labels = labels
 
-    def __preprocess(self):
-        return
+    def __preprocess(self, data: data_class, train:bool):
+        df = data.filter_features(base=False, feature=True, ratio=True)
+        # TODO: x, y
+        x = ''
+        y = ''
+        # TODO: train_test_split(df, y, test_size=0.3, random_state=42, stratify=y)
+        if train:
+            train_x, train_y, test_x, test_y = train_test_split(df, y, test_size=0.3, random_state=42, stratify=y)
+            output = (train_x, train_y, test_x, test_y)
+        else:
+            output = (x, y)
+        return df, output
     
-    def load(self):
+    def load(self) -> None:
+        #TODO: load model from file, if fail raise error
+        #TODO: load other objects we might need (scaler, normalizer, pca??)
         return
 
-    def train(self):
+    def create_model(self):
+        self.model = RandomForestClassifier(n_estimators=300, random_state=7, n_jobs=-1)
+        return self.model
+    
+    def train(self, data: data_class) -> None:
+        df, (x, y) = self.__preprocess(self, data, True)
+
+        # create model if there is none
+        if self.model == None:
+            self.create_model()
+
+        self.model.fit(x,y)
         pass
 
     def predict(self, data: data_class) -> list:
-        self.df = data.filter_features(base=False, feature=True, ratio=True)
-        self.__preprocess()
-        return
+        df = self.__preprocess(self, data)
+        if self.model == None:
+            self.load()
+        prediction = self.model.predict(df)
+        return prediction
--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import asyncio
 import logging
 
+import aiohttp
 import requests
 
 from config import app, detector_api, token
-
-from MachineLearning.model import model
 from MachineLearning.data import data_class
+from MachineLearning.model import model
 
-labels = [
+LABELS = [
     'Real_Player', 'Smithing_bot', 'Mining_bot', 
     'Magic_bot', 'PVM_Ranged_bot',  
     'Fletching_bot', 'PVM_Melee_bot', 'Herblore_bot',
@@ -20,7 +20,20 @@ labels = [
     'Zalcano_bot'
 ]
 
-ml = model(labels)
+ml = model(LABELS)
+
+async def loop_request(url, json):
+    i = 1
+    data = []
+    while True:
+        url = f'{detector_api}/v1/player/bulk?token={token}&row_count=100000&page={i}'
+        logging.debug(url)
+        res = requests.post(url, json=json).json()
+        data += res
+        if len(res) == 0:
+            break
+        i += 1
+    return data
 
 @app.on_event('startup')
 async def initial_task():
@@ -40,13 +53,29 @@ async def train():
 
 @app.post("/train")
 async def train():
-    if ml.model is None:
-        ml.load()
+    #TODO: token
+
+    #TODO: get all labels & id's
+    url = f'{detector_api}/v1/label?token={token}'
+    logging.debug(url)
+    data = requests.get(url).json()
+
+    labels = [d for d in data if d['label'] in LABELS]
+    label_ids = [l['id'] for l in labels]
+    #TODO: get all labeled users from db
+    label_input = {}
+    label_input['label_id'] = label_ids
+    
+    data = loop_request(url, label_input)
+
+    print(len(data))
+    #TODO: get all hiscore data latest for those users from db
     return {'ok': 'ok'}
 
 async def get_player_hiscores():
     logging.debug('getting data')
     url = f'{detector_api}/v1/prediction/data?token={token}'
+    logging.debug(url)
     data = requests.get(url).json()
 
     # if there is no data wait and try to see if there is new data
