349cac5634d9d012be9aecfab660948e0cf0ffb7
extreme4all <40169115+extreme4all@users.noreply.github.com>
2022-02-27 19:45:35 +0100

    rewrite (#26)
    * rewrite
    
    * training successfull on docker
    
    * almost working
    
    * it works
    
    * auto startup

--- api/app.py
+++ api/app.py
@@ -1,160 +1,202 @@
 import asyncio
 import logging
+import time
+from typing import List
 
-import aiohttp
-from fastapi import BackgroundTasks, HTTPException
+import pandas as pd
+import requests
+from fastapi import HTTPException
+from pydantic import BaseModel
+from sklearn.model_selection import train_test_split
 
 from api import config
-from api.cogs import requests
-from api.MachineLearning.data import data_class
-from api.MachineLearning.model import model
-
-app = config.app
+from api.cogs import predict
+from api.cogs import requests as req
+from api.MachineLearning import classifier, data
 
 logger = logging.getLogger(__name__)
 
-LABELS = [
-    'Real_Player',
-    'PVM_Melee_bot',
-    'Smithing_bot',
-    'Magic_bot',
-    'Fishing_bot',
-    'Mining_bot',
-    'Crafting_bot',
-    'PVM_Ranged_Magic_bot',
-    'PVM_Ranged_bot',
-    'Hunter_bot',
-    'Fletching_bot',
-    # 'Clue_Scroll_bot',
-    'LMS_bot',
-    'Agility_bot',
-    'Wintertodt_bot',
-    'Runecrafting_bot',
-    'Zalcano_bot',
-    'Woodcutting_bot',
-    'Thieving_bot',
-    'Soul_Wars_bot',
-    'Cooking_bot',
-    'Vorkath_bot',
-    'Barrows_bot',
-    'Herblore_bot',
-    'Zulrah_bot'
-]
+app = config.app
 
-ml = model(LABELS)
+binary_classifier = classifier.classifier('binaryClassifier').load()
+multi_classifier = classifier.classifier('multiClassifier').load()
 
 
-async def stage_and_train(token: str):
-    # request labels
-    url = f'{config.detector_api}/v1/label?token={token}'
+class name(BaseModel):
+    id: int
+    name: str
 
-    # logger
-    async with aiohttp.ClientSession() as session:
-        data = await requests.get_request(session, url)
 
-    # filter labels
-    labels = [d for d in data if d['label'] in LABELS]
-    
-    # memory cleanup
-    del url, data
-
-    # get players
-    base = f'{config.detector_api}/v1/player/bulk?token={token}'
-    urls = [f'{base}&label_id={label["id"]}' for label in labels]
-    players = await requests.batch_request(urls)
-
-    # memory cleanup
-    del base, urls
-
-    # get hiscores
-    base = f'{config.detector_api}/v1/hiscore/Latest/bulk?token={token}'
-    urls = [f'{base}&label_id={label["id"]}' for label in labels]
-    hiscores = await requests.batch_request(urls)
-
-    # memory cleanup
-    del base, urls
-
-    # hiscores dict
-    hiscores = data_class(hiscores)
-
-    ml.train(players, labels, hiscores)
-
-    # memory cleanup
-    del players, labels, hiscores
-    logger.debug('ML trained')
-
+@app.on_event('startup')
+async def initial_task():
+    global binary_classifier, multi_classifier
+    if binary_classifier is None or multi_classifier is None:
+        binary_classifier = classifier.classifier('binaryClassifier')
+        multi_classifier = classifier.classifier('multiClassifier')
+        await train(config.secret_token)
+    await manual_startup(config.secret_token)
     return
 
-async def get_player_hiscores():
-    logger.debug('getting data')
-    url = f'{config.detector_api}/v1/prediction/data?token={config.token}&limit=50000'
-    logger.debug(url)
-    
-    async with aiohttp.ClientSession() as session:
-        data = await requests.get_request(session, url)
-        
-    # if there is no data wait and try to see if there is new data
-    if len(data) == 0:
-        logger.debug('no data to predict')
-        await asyncio.sleep(600)
-        return asyncio.create_task(get_player_hiscores())
-
-    # clean & filter data
-    data = data_class(data)
-    
-    # make predictions
-    predictions = ml.predict(data) # dataframe
-    predictions = predictions.to_dict(orient='records') # list of dict
-
-    # post predictions
-    url = f'{config.detector_api}/v1/prediction?token={config.token}'
-    logger.debug(url)
-    async with aiohttp.ClientSession() as session:
-        resp = await requests.post_request(session, url, predictions)
-
-    return asyncio.create_task(get_player_hiscores())
-
-# @app.on_event('startup')
-# async def initial_task():
-#     asyncio.create_task(get_player_hiscores())
-#     return
 
 @app.get("/")
-async def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"detail": "hello worldz"}
+
 
 @app.get("/startup")
-async def manual_startup(secret:str):
-    #TODO: verify token
+async def manual_startup(secret: str):
+    """
+        start predicting
+    """
+    # secret token for api's to talk to eachother
     if secret != config.secret_token:
-        raise HTTPException(status_code=404, detail=f"insufficient permissions")
+        raise HTTPException(
+            status_code=404, detail=f"insufficient permissions")
 
-    asyncio.create_task(get_player_hiscores())
-    return {'ok': 'Predictions have been started.'}
+    while True:
+        # endpoint that we are going to use
+        data_url = f'{config.detector_api}/v1/prediction/data?token={config.token}&limit=50000'
+        output_url = f'{config.detector_api}/v1/prediction?token={config.token}'
+
+        hiscores = req.request([data_url])
+        hiscores = pd.DataFrame(hiscores)
+        if len(hiscores) == 0:
+            logger.debug('No data: sleeping')
+            time.sleep(600)
+            continue
+
+        names = hiscores[
+            ["Player_id", "name"]
+        ].rename(columns={"Player_id": "id"})
+        hiscores = hiscores[[c for c in hiscores.columns if c != "name"]]
+
+        output = predict.predict(
+            hiscores, names, binary_classifier, multi_classifier)
+        resp = requests.post(output_url, json=output)
+
+        if resp.status_code != 200:
+            print(resp.text)
+            time.sleep(600)
+    return {'detail': 'ok'}
+
 
 @app.get("/load")
-async def load(secret:str):
-    #TODO: verify token
+async def load(secret: str):
+    global binary_classifier, multi_classifier
+    """
+        load the latest model
+    """
     if secret != config.secret_token:
-        raise HTTPException(status_code=404, detail=f"insufficient permissions")
+        raise HTTPException(
+            status_code=404, detail=f"insufficient permissions")
 
-    if ml.model is None:
-        ml.model = ml.load('model')
-    return {'ok': 'ok'}
+    binary_classifier = binary_classifier.load()
+    multi_classifier = multi_classifier.load()
+    return {'detail': 'ok'}
+
 
 @app.get("/predict")
-async def predict(secret:str):
-    #TODO: verify token
+async def predict_player(secret: str, hiscores, name: name) -> List[dict]:
+    """
+        predict one player
+    """
     if secret != config.secret_token:
-        raise HTTPException(status_code=404, detail=f"insufficient permissions")
+        raise HTTPException(
+            status_code=404, detail=f"insufficient permissions")
+    name = pd.DataFrame(name.dict())
+    output = predict.predict(
+        hiscores, name, binary_classifier, multi_classifier)
+    return output
 
-    return 
 
 @app.get("/train")
-async def train(secret: str, token: str, background: BackgroundTasks):
+async def train(secret: str):
+    """
+        train a new model
+    """
     if secret != config.secret_token:
-        raise HTTPException(status_code=404, detail=f"insufficient permissions")
+        raise HTTPException(
+            status_code=404, detail=f"insufficient permissions")
 
-    background.add_task(stage_and_train, token)
+    # api endpoints
+    label_url = f'{config.detector_api}/v1/label?token={config.token}'
+    player_url = f'{config.detector_api}/v1/player/bulk?token={config.token}'
+    hiscore_url = f'{config.detector_api}/v1/hiscore/Latest/bulk?token={config.token}'
 
-    return {'ok': 'Training has begun.'}
+    # request labels
+    labels = req.request([label_url])
+
+    # request players
+    player_urls = [
+        f'{player_url}&label_id={label["id"]}' for label in labels
+        if label["label"] in config.LABELS
+    ]
+    players = req.request(player_urls)
+
+    # request hiscore data
+    hiscore_urls = [
+        f'{hiscore_url}&label_id={label["id"]}' for label in labels
+        if label["label"] in config.LABELS
+    ]
+    hiscores = req.request(hiscore_urls)
+
+    # parse hiscoreData
+    hiscoredata = data.hiscoreData(hiscores)
+    del hiscores
+
+    # get the desired features
+    features = hiscoredata.features()
+    del hiscoredata
+
+    ###############################################################
+    # get players with binary target
+    player_data = data.playerData(players, labels).get(binary=True)
+
+    # merge features with target
+    features_labeled = features.merge(
+        player_data, left_index=True, right_index=True)
+
+    # create train test data
+    x, y = features_labeled.iloc[:, :-1], features_labeled.iloc[:, -1]
+    train_x, test_x, train_y, test_y = train_test_split(
+        x, y, test_size=0.2, random_state=42, stratify=y
+    )
+
+    # train & score the model
+    binary_classifier.fit(train_x, train_y)
+    binary_classifier.score(test_y, test_x)
+
+    # save the model
+    binary_classifier.save()
+    ###############################################################
+
+    # get players with multi target
+    player_data = data.playerData(players, labels).get(binary=False)
+
+    # merge features with target
+    features_labeled = features.merge(
+        player_data, left_index=True, right_index=True
+    )
+
+    # we need at least 100 users
+    to_little_data_labels = pd.DataFrame(
+        features_labeled.iloc[:, -1].value_counts()
+    ).query('target < 100').index
+    mask = ~(features_labeled['target'].isin(to_little_data_labels))
+    features_labeled = features_labeled[mask]
+
+    # create train test data
+    x, y = features_labeled.iloc[:, :-1], features_labeled.iloc[:, -1]
+    train_x, test_x, train_y, test_y = train_test_split(
+        x, y, test_size=0.2, random_state=42, stratify=y
+    )
+
+    # train & score the model
+    multi_classifier.fit(train_x, train_y)
+    multi_classifier.score(test_y, test_x)
+
+    # save the model
+    multi_classifier.save()
+    ###############################################################
+
+    return {'detail': 'ok'}
