118ea8d114dc3b08206d1d6777f2af505eb4c628
y c <40169115+extreme4all@users.noreply.github.com>
2022-04-07 23:36:31 +0200

    removed time out

--- api/cogs/requests.py
+++ api/cogs/requests.py
@@ -7,25 +7,27 @@ import time
 logger = logging.getLogger(__name__)
 
 def request(
-    urls: List[str], retry: int = 3, sleep: int = 0, timeout: int = 10
+    urls: List[str], retry: int = 3, sleep: int = 1
 ) -> List[dict]:
     """
-    request data from urls
+    request data from urls.
+
     :param urls: list of urls
     :param retry: number of retries
     :param sleep: number of seconds to sleep
-    :return: list of dicts
+    :return: list of dicts.
     """
     output = []
     for url in urls:
         logger.debug(url.replace(config.token, "***"))
         try:
-            data = requests.get(url, timeout=timeout)
+            data = requests.get(url)
             data = data.json()
-        except:
+        except Exception as e:
+            logger.exception(e)
             if retry > 0:
                 time.sleep(sleep)
-                data = request([url], retry - 1, sleep, timeout)
+                data = request([url], retry - 1, sleep)
             else:
                 data = []
         output.extend(data)
